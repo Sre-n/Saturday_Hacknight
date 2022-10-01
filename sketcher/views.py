c9a00946ae144931e99e91a17b331a72e16c374a
unniznd <au6695@gmail.com>
2022-10-01 22:56:08 +0530

    fixed

--- sketcher/views.py
+++ sketcher/views.py
@@ -5,26 +5,16 @@ from django.contrib import messages
 from sketcher.forms import ImageForm
 from sketcher.sketch import sketch
 
-import numpy as np
-import cv2
-
-from PIL import Image
 
 def home(request):
     if request.method == "POST":
         form = ImageForm(request.POST,request.FILES)
         if form.is_valid():
             image = form.cleaned_data.get("image")
-            file_name = default_storage.save(image.name, image)
-            fh = default_storage.open(file_name, "w")
-            img = cv2.imdecode(np.fromstring(fh.read(), np.uint8), cv2.IMREAD_UNCHANGED)
-            sketched_img = sketch(img)
-            final_img = Image.fromarray(sketched_img)
-            fh = default_storage.save(file_name, final_img)
-            fh.close()
-            image_url = default_storage.url(file_name)
-            print(image_url)
+            image_name = default_storage.save(image.name.strip(), image)
+            image_url = default_storage.url(image_name)
             try:
+                sketch("media/"+image_name)
                 messages.success(request,"Sketch successful!!")
                 return render(request,'home.html',{'form':form,'image_url':image_url})
             except:
