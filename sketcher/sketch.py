72c69fb7546dd48769d30f71af81cc2452a31099
unniznd <au6695@gmail.com>
2022-10-01 22:50:47 +0530

    fixes

--- sketcher/sketch.py
+++ sketcher/sketch.py
@@ -1,12 +1,10 @@
 import cv2
 
-
-
-def sketch(image_url):
-    image = cv2.imread("./"+image_url)  
+def sketch(image):
+    
     grey_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     invert = cv2.bitwise_not(grey_img) 
     blur = cv2.GaussianBlur(invert, (21, 21), 0)
     invertedblur = cv2.bitwise_not(blur)
     sketch = cv2.divide(grey_img, invertedblur, scale=256.0)
-    cv2.imwrite("./"+image_url, sketch) 
+    return sketch
